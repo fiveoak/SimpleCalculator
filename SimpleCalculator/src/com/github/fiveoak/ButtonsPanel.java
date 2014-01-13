660a136101c5208d3d2640486827919736876dba
fiveoak <fiveofakind@gmail.com>
2014-01-13 11:54:59 -0500

    added all digits and some control buttons

--- SimpleCalculator/src/com/github/fiveoak/ButtonsPanel.java
+++ SimpleCalculator/src/com/github/fiveoak/ButtonsPanel.java
@@ -1,5 +1,8 @@
 package com.github.fiveoak;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -33,11 +36,21 @@ public class ButtonsPanel {
      */
     private static final int NUM_DIGIT_BUTTONS = 10;
 
+    private JButton periodButton;
+    private JButton equalsButton;
+    private JButton divButton;
+    private JButton multButton;
+    private JButton addButton;
+    private JButton subButton;
+    private JButton leftParensButton;
+    private JButton rightParensButton;
+    private JButton clearButton;
+
     /**
      * Constructor.
      */
     ButtonsPanel() {
-        buttonsJPanel = new JPanel();
+        buttonsJPanel = new JPanel(new GridBagLayout());
         setupAndAddDigitButtons();
     }
 
@@ -47,12 +60,103 @@ public class ButtonsPanel {
      */
     private void setupAndAddDigitButtons() {
         calculatorDigitButtons = new JButton[NUM_DIGIT_BUTTONS];
+        periodButton = new JButton(".");
+        equalsButton = new JButton("=");
+        divButton = new JButton("/");
+        multButton = new JButton("*");
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        leftParensButton = new JButton("(");
+        rightParensButton = new JButton(")");
+        clearButton = new JButton("CLR");
+
+
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
 
         for (int i = 0; i < NUM_DIGIT_BUTTONS; i++) {
             calculatorDigitButtons[i] = new JButton();
             calculatorDigitButtons[i].setText(Integer.toString(i));
-            buttonsJPanel.add(calculatorDigitButtons[i]);
         }
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[7], gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[8], gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[9], gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(divButton, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[4], gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[5], gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[6], gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(multButton, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[1], gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[2], gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[3], gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(subButton, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(calculatorDigitButtons[0], gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(periodButton, gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(equalsButton, gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(addButton, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(leftParensButton, gridBagConstraints);
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(rightParensButton, gridBagConstraints);
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.weightx = 0.5;
+        buttonsJPanel.add(clearButton, gridBagConstraints);
+
+
     }
 
     /**
@@ -71,5 +175,17 @@ public class ButtonsPanel {
      */
     public JButton[] getDigitButtons() {
         return calculatorDigitButtons;
+    }
+
+    /**
+     * Returns an array of references to the calculator control buttons.
+     * @return Returns the following buttons in order: periodButton,
+     * equalsButton, divButton, multButton, addButton,
+     * subButton, leftParensButton, rightParensButton, clearButton.
+     */
+    public JButton[] getControlButtons() {
+        return new JButton[]
+                {periodButton, equalsButton, divButton, multButton, addButton,
+                subButton, leftParensButton, rightParensButton, clearButton};
     }
 }
